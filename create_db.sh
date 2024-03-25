92be6654f7a7864008a17a86a10e60aaccf33d9d
Allie <alliesafronova@gmail.com>
2024-03-25 22:42:10 +0700

    DB creation update

--- create_db.sh
+++ create_db.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 USER=allie
-DB_NAME=worldcup
-PSQL="psql --username=$USER --dbname=$DB_NAME --no-align --tuples-only -f"
+DEFAULT_DB_NAME=postgres
+DB_NAME=$1
+if [[ -z $DB_NAME ]]
+then
+    DB_NAME=worldcup
+fi
+PSQL="psql --username=$USER --dbname=$DEFAULT_DB_NAME --no-align --tuples-only -c"
+DB_EXISTS=$($PSQL "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'")
+if [[ -z $DB_EXISTS ]]
+then
+    echo "$($PSQL "CREATE DATABASE $DB_NAME")"
+fi
 
+
+PSQL="psql --username=$USER --dbname=$DB_NAME --no-align --tuples-only -f"
 echo "$($PSQL create_db.sql)"
--- insert_data.sh
+++ insert_data.sh
@@ -5,6 +5,7 @@ then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
 else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
-fi
+fi+ [] Your teams table should have a team_id column that is a type of SERIAL and is the primary key, and a name column that has to be UNIQUE
+
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
