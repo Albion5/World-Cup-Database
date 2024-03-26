b91dd0df8f778d7f6d255a1e32ff5b6d08fd59e1
Allie <alliesafronova@gmail.com>
2024-03-26 11:50:18 +0700

    Initital files

--- insert_data.sh
+++ insert_data.sh
@@ -5,7 +5,6 @@ then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
 else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
-fi+ [] Your teams table should have a team_id column that is a type of SERIAL and is the primary key, and a name column that has to be UNIQUE
-
+fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
